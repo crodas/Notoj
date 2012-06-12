71ac1ce9e6cf02690776418103aabc4c1fe8307d
César D. Rodas <crodas@php.net>
2012-06-12 14:29:44 -0400

    New Autoloader

--- lib/Notoj/Loader.php
+++ lib/Notoj/Loader.php
@@ -1,26 +1,44 @@
 <?php
-
-spl_autoload_register(function ($class) {
+$call = 0;
+$load = 0;
+spl_autoload_register(function ($class) use (&$call, &$load) {
     /*
         This array has a map of (class => file)
     */
     static $classes = array (
-  'Notoj\\Notoj' => '/Notoj.php',
-  'Notoj\\Tokenizer' => '/Tokenizer.php',
-  'Notoj_yyToken' => '/Parser.php',
-  'Notoj_yyStackEntry' => '/Parser.php',
-  'Notoj_Parser' => '/Parser.php',
-  'Notoj\\ReflectionFunction' => '/ReflectionFunction.php',
-  'Notoj\\ReflectionProperty' => '/ReflectionProperty.php',
-  'Notoj\\ReflectionMethod' => '/ReflectionMethod.php',
-  'Notoj\\ReflectionClass' => '/ReflectionClass.php',
-  'Notoj\\ReflectionObject' => '/ReflectionObject.php',
+  'notoj\\notoj' => '/Notoj.php',
+  'notoj\\tokenizer' => '/Tokenizer.php',
+  'notoj_yytoken' => '/Parser.php',
+  'notoj_yystackentry' => '/Parser.php',
+  'notoj_parser' => '/Parser.php',
+  'notoj\\reflectionfunction' => '/ReflectionFunction.php',
+  'notoj\\reflectionproperty' => '/ReflectionProperty.php',
+  'notoj\\reflectionmethod' => '/ReflectionMethod.php',
+  'notoj\\reflectionclass' => '/ReflectionClass.php',
+  'notoj\\reflectionobject' => '/ReflectionObject.php',
+);
+    static $deps    = array (
 );
 
+    $class = strtolower($class);
     if (isset($classes[$class])) {
-        require __DIR__  . $classes[$class];
+        $call++;
+        $load++;
+        if (!empty($deps[$class])) {
+            foreach ($deps[$class] as $zclass) {
+                if (!class_exists($zclass, false) && !interface_exists($zclass, false)) {
+                    $load++;
+                    require __DIR__  . $classes[$zclass];
+                }
+            }
+        }
+
+        if (!class_exists($class, false) && !interface_exists($class, false)) {
+            require __DIR__  . $classes[$class];
+        }
         return true;
     }
 
     return false;
 });
+
