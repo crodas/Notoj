665384f5b9ddf0e6f5564e59c7fcd89ee6753163
César D. Rodas <crodas@php.net>
2015-01-24 17:34:35 -0300

    Changing code for PHP5.3

--- tests/SimpleTest.php
+++ tests/SimpleTest.php
@@ -367,7 +367,7 @@ class simpletest extends \phpunit_framework_testcase
         $args = $annotations[0]->GetArgs();
         $this->assertTrue($args[0] instanceof \Notoj\Annotation\Annotation);
         $this->AssertEquals('foobar', $args[0]->getName());
-        $this->AssertEquals(['foobar'], $args[0]->getArgs());
+        $this->AssertEquals(array('foobar'), $args[0]->getArgs());
         $this->AssertEquals('foobar', $args[1]);
     }
 }
